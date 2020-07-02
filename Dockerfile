e0471598ffc4f0b6ad85598fc10e0d88afc3f65d
Alberto <alberto@docker.com>
2020-07-02 18:55:26 +0200

    Add SSL for wss connections

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN pip install -r requirements.txt
 
 # Configure nginx
 COPY nginx /etc/nginx/
+
 # Forward request and error logs to docker log collector
 RUN ln -sf /dev/stdout /var/log/nginx/access.log && ln -sf /dev/stderr /var/log/nginx/error.log
 COPY run.sh server.py ./
--- client.py
+++ client.py
@@ -3,16 +3,22 @@
 # WS client example
 
 import asyncio
+import os
+import pathlib
+import ssl
 import uuid
 
 import websockets
 
-TOKEN = "eyJraWQiOiJGQjR2TEMxanhlXC9WQjJcL0lpOTRncTFzMTdYVHc3UTVodElQNklGRFg1TjQ9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJjOGMxM2Q2Yi0xZGVjLTQ3NzktYTEyYy1kYmY3OTE1YzczNGUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLXdlc3QtMi5hbWF6b25hd3MuY29tXC91cy13ZXN0LTJfdHBlOGNMV2F3IiwiY29nbml0bzp1c2VybmFtZSI6ImM4YzEzZDZiLTFkZWMtNDc3OS1hMTJjLWRiZjc5MTVjNzM0ZSIsImdpdmVuX25hbWUiOiJBZG1pbiIsImF1ZCI6IjI3dm9sMmNlZ2g5Nm5vYmRsMWcxZGxsMWNtIiwiZXZlbnRfaWQiOiJkYzU1OWYwYi04Njg4LTQ3YjUtODdjZC0wODYxNjZlNDhiMzQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTU5MzcwMTEyNCwiZXhwIjoxNTkzNzA0NzI0LCJpYXQiOjE1OTM3MDExMjQsImZhbWlseV9uYW1lIjoiSW1lcml0IiwiZW1haWwiOiJhZG1pbkBpbWVyaXQuY29tIn0.RmzsGo2mLFvnNnWm6SQ5KCh1JdPG4OEkAruk5Tqamt2nuSr6y9R0xMu8sqox4hM-BPyPgtnYkzHo_ga7FxT_sxpWgh-ErigpA5cWezZfceAUkQoh7xP3OOUzM45nKhy7kFocH-TDbe5n6cwP0vILo__J7GQWbk6cSZVFS1GAz_sHxzQ_fw01AJcPc9f4BBFtW2ztJKnjeZT8rujNoigbM744cVAQ4Tu6e1sD4ZJ5yplyMXJM8KEw6oH_48TtgORoJAbbcZdEU2t9SWuVmQyBdVc2R1-wTvpp2eaSFL9j9TkV1nMUVUcENUyhDDjsk9Npq6hdqTFrCACMcgIiOZ5-Xw"
+JWT_TOKEN = "FILL ME"
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+localhost_pem = pathlib.Path(os.path.join(os.path.dirname(__file__), "nginx/ssl/cert.pem"))
+ssl_context.load_verify_locations(localhost_pem)
 
 
 async def hello():
-    uri = f"ws://localhost:8888/socket.io/?token={TOKEN}"
-    async with websockets.connect(uri) as websocket:
+    uri = f"wss://localhost:8888/socket.io/?token={JWT_TOKEN}"
+    async with websockets.connect(uri, ssl=ssl_context) as websocket:
         for i in range(20):
             random_string = str(uuid.uuid4())
             await websocket.send(random_string)
